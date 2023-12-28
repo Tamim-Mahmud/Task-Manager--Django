803b255ddd25de0d035cf86dbc9a8ac0d86ff3c6
TamimMahmud <tmahmud.jnubd@gmail.com>
2023-12-28 16:17:02 +0600

    models created

--- task_manager/tasks/models.py
+++ task_manager/tasks/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 
 # Create your models here.
+
+class Tasks_list(models.Model):
+    PRIORITY_CHOICES = [
+        (1, 'Low'),
+        (2, 'Medium'),
+        (3, 'High'),
+    ]
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    image = models.ImageField(upload_to='todo_images/', blank=True, null=True)
+    due_date = models.DateTimeField()
+    priority = models.IntegerField(choices=PRIORITY_CHOICES, default=2)  # Default to Medium
+    is_complete = models.BooleanField(default=False)
+    creation_date = models.DateTimeField(auto_now_add=True)
+    last_update_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return (f"{self.title} {self.due_date}")
